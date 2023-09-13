802a61862180b5f6efd406f93cb24a7103110171
emanusantos <emanuel.lmds1@gmail.com>
2023-09-12 21:20:33 -0300

    chore: folder structure

--- src/shared/routes/AboutView.vue
+++ src/shared/routes/AboutView.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <HelloWorld msg="Test" />
   </div>
 </template>
 
